35ea1d43e793d4b551633fdedd81eed3b7f494eb
Derek Wolpert <me@derekwolpert.com>
2022-09-20 16:01:41 -0400

    add marked as a dependency, and export parsed markdown files content from index file

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-export const pages = [
+import { readFile } from 'fs/promises'
+import { fileURLToPath } from 'url'
+import { marked } from 'marked'
+
+const getFilePath = file => (
+  fileURLToPath(new URL(`./pages/${file}.md`, import.meta.url))
+)
+
+const getFileContent = async file => (
+  (await readFile(getFilePath(file))).toString()
+)
+
+const mdToHtml = async page => {
+  const file = page.replaceAll(' ', '')
+  const markdown = await getFileContent(file)
+  const html = marked.parse(markdown)
+
+  return { page, html }
+}
+
+const pages = [
+  'Getting Started',
   'Applications',
   'Environments',
   'Builds',
@@ -11,8 +32,10 @@ export const pages = [
   'Stacks',
   'Logs',
   'Security',
-  'Local',
+  'Local Development',
   'Manifests',
   'Logic',
   'Workflows'
 ]
+
+export default await Promise.all(pages.map(page => mdToHtml(page)))
