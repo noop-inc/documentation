26d53cc53983a62e9ad7e30f67380f91e55ebb34
Joe Kendall <joe.kendall+work@noop.dev>
2023-11-16 11:43:39 -0500

    temporarily inlining external package until it has support for pagefind subdir config OR we publish our own fork. (#53)
    Co-authored-by: Joe Kendall <joe.kendall@noop.dev>

---
+++ lib/astro-pagefind/components/Search.ts
@@ -0,0 +1,5 @@
+// Temporarily inlined from: https://github.com/shishkin/astro-pagefind
+// Need functionality from PR: https://github.com/shishkin/astro-pagefind/pull/58
+// TODO: figure out how to avoid this:
+// @ts-expect-error
+export { default } from "./Search.astro";
---
+++ lib/astro-pagefind/pagefind.ts
@@ -0,0 +1,71 @@
+// Temporarily inlined from: https://github.com/shishkin/astro-pagefind
+// Need functionality from PR: https://github.com/shishkin/astro-pagefind/pull/58
+
+import type { AstroIntegration } from "astro";
+import { fileURLToPath } from "node:url";
+import { execSync } from "child_process";
+import sirv from "sirv";
+
+export default function pagefind(pagefindConfig: { site?: string; outputSubdir?: string } = {}): AstroIntegration {
+  const {site, outputSubdir = 'pagefind'} = pagefindConfig
+
+  process.env.PAGEFIND_OUTPUT_SUBDIR = outputSubdir
+  process.env.PAGEFIND_SITE = site
+
+  let outDir: string;
+  return {
+    name: "pagefind",
+    hooks: {
+      "astro:config:setup": ({ config }) => {
+        if (config.output === "server") {
+          console.warn(
+            "Output type `server` does not produce static *.html pages in its output and thus will not work with astro-pagefind integration.",
+          );
+          return;
+        }
+
+        if (config.adapter?.name.startsWith("@astrojs/vercel")) {
+          outDir = fileURLToPath(new URL(".vercel/output/static/", config.root));
+        } else if (config.adapter?.name === "@astrojs/cloudflare") {
+          outDir = fileURLToPath(new URL(config.base?.replace(/^\//, ""), config.outDir));
+        } else if (config.adapter?.name === "@astrojs/node" && config.output === "hybrid") {
+          outDir = fileURLToPath(config.build.client!);
+        } else {
+          outDir = fileURLToPath(config.outDir);
+        }
+      },
+      "astro:server:setup": ({ server }) => {
+        if (!outDir) {
+          console.warn(
+            "astro-pagefind could reliably determine the output directory. Search assets will not be served.",
+          );
+          return;
+        }
+
+        const serve = sirv(outDir, {
+          dev: true,
+          etag: true,
+        });
+        server.middlewares.use((req, res, next) => {
+          if (req.url?.startsWith(`/${outputSubdir}/`)) {
+            serve(req, res, next);
+          } else {
+            next();
+          }
+        });
+      },
+      "astro:build:done": () => {
+        if (!outDir) {
+          console.warn(
+            "astro-pagefind could reliably determine the output directory. Search index will not be built.",
+          );
+          return;
+        }
+        const cmd = `npx pagefind --site "${outDir}" --output-subdir "${outputSubdir}"`;
+        execSync(cmd, {
+          stdio: [process.stdin, process.stdout, process.stderr],
+        });
+      },
+    },
+  };
+}
