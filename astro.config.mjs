c6145e3cba54212970008e337c2cf7995d7d3186
Joe Kendall <joe.kendall@noop.dev>
2023-11-16 11:39:46 -0500

    temporarily inlining external package until it has support for pagefind subdir config OR we publish our own fork.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config'
-import pagefind from "astro-pagefind";
+import { defineConfig } from 'astro/config';
+import pagefind from "./lib/astro-pagefind/pagefind.ts"
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-      inlineStylesheets: 'always',
-      assets: 'assets/docs/_astro'
-  },
-    
-    integrations: [tailwind(), pagefind()]
+    build: {
+        inlineStylesheets: 'always',
+        assets: 'assets/docs/_astro'
+    },
+
+    integrations: [tailwind(), pagefind({site: 'dist', outputSubdir: 'assets/docs/pagefind'})]
 })
